696df785ad2a8a3c367340d0107c05231e84d30d
CameronChurchwell <cameronchurchwell2024@u.northwestern.edu>
2024-02-05 21:23:48 -0500

    pyproject

--- setup.py
+++ setup.py
@@ -8,10 +8,6 @@ os.environ['CXX'] = 'g++-11'
 os.environ['CC'] = 'gcc-11'
 
 
-with open('README.md', encoding='utf-8') as file:
-    long_description = file.read()
-
-
 modules = [
     CUDAExtension(
         'viterbi',
@@ -26,22 +22,8 @@ modules = [
 
 
 setup(
-    name='torbi',
-    description='Optimized Viterbi decoding and fast approximations',
-    version='0.0.1',
-    author='Max Morrison',
-    author_email='maxrmorrison@gmail.com',
-    url='https://github.com/maxrmorrison/torbi',
     ext_modules=modules,
     cmdclass={'build_ext': BuildExtension},
     include_dirs=[np.get_include(), 'torbi'],
-    setup_requires=['numpy', 'pybind11', 'torch'],
-    install_requires=['numpy', 'torch', 'torchutil', 'yapecs'],
-    extras_require={'evaluate': ['librosa', 'torchaudio']},
     packages=find_packages(),
-    package_data={'torbi': ['assets/*', 'assets/*/*']},
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    keywords=['decode', 'sequence', 'torch', 'Viterbi'],
-    classifiers=['License :: OSI Approved :: MIT License'],
-    license='MIT')
+)
